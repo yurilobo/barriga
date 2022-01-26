7192757453f28e892be06d1bc380e1b63c5970f9
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-26 11:38:28 -0300

    Gerenciamento de massa

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -49,6 +49,17 @@ describe('Should test at a functional level',()=>{
     it('Should get balance', ()=>{
         cy.get(loc.MENU.HOME).click()
         cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain','534,00')
+
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
     })
     it('Should remove a transaction', ()=>{
         cy.get(loc.MENU.EXTRATO).click()
