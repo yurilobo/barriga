7dd84b793cfedaa9f6477681de368a480b86801d
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-28 17:35:20 -0300

    Inserindo uma conta

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -10,7 +10,7 @@ describe('Should test at a functional level',()=>{
         //cy.resetApp()
     })
     it('Should create an account',()=>{
-        cy.request({
+       cy.request({
             method: 'POST',
             url: 'https://barrigarest.wcaquino.me/signin',
             body:{
@@ -19,6 +19,23 @@ describe('Should test at a functional level',()=>{
                 senha: "12345"
             }
         }).its('body.token').should('not.be.empty')
+            .then(token=>{
+                cy.request({
+                    method: 'POST',
+                    headers: {Authorization: `JWT ${token}`},
+                    url: 'https://barrigarest.wcaquino.me/contas',
+                    body:{
+                        nome:"Conta via rest"
+                    }
+                }).as('response')
+            })
+            cy.get('@response').then(res=>{
+                expect(res.status).to.be.equal(201)
+                expect(res.body).to.have.property('id')
+                expect(res.body).to.have.property('nome', 'Conta via rest')
+            })
+
+        
         
     })
     it('Should update an accont', ()=>{
