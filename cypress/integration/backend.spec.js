a4f52a5b3d7fef832b1df8abe49ba106fee0ac14
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-27 23:58:32 -0300

    Login

---
+++ cypress/integration/backend.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress"/>
+
+
+describe('Should test at a functional level',()=>{
+    before(()=>{
+        //cy.login('yuri@1', '12345')
+            
+    })
+    beforeEach(()=>{
+        //cy.resetApp()
+    })
+    it('Should create an account',()=>{
+        cy.request({
+            method: 'POST',
+            url: 'https://barrigareact.wcaquino.me/signin',
+            body:{
+                email: "yuri@1",
+                redirecionar: false,
+                senha: "12345"
+            }
+        }).then(res => console.log(res))
+        
+    })
+    it('Should update an accont', ()=>{
+        
+    })
+    it('Should not crate an account with same name',()=>{
+        
+    })
+    it('Should create a transaction',()=>{
+        
+    })
+    it('Should get balance', ()=>{
+       
+    })
+    it('Should remove a transaction', ()=>{
+        
+    })
+})
+
