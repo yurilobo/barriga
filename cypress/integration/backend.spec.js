16a6a4339ca82d2237dddea42d025901f1914420
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2022-01-30 22:57:37 -0300

    ajustes e função moment

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress"/>
 
+
+
 let token
 describe('Should test at a functional level', () => {
     before(() => {
@@ -51,7 +53,7 @@ describe('Should test at a functional level', () => {
         cy.get('@response').its('status').should('be.equal', 200)
         
     })
-    it.only('Should not crate an account with same name', () => {
+    it('Should not crate an account with same name', () => {
         
         cy.request({
             url: '/contas',
@@ -70,14 +72,92 @@ describe('Should test at a functional level', () => {
             expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
         })
     })
-    it('Should create a transaction', () => {
+    it.skip('Should create a transation', () => {
+        cy.getContaByName('Conta para movimentacoes')
+            .then(contaId => {
+                cy.request({
+                    method: 'POST',
+                    url: '/transacoes',
+                    //headers: { Authorization: `JWT ${token}` },
+                    body: {
+                        conta_id: contaId,
+                        data_pagamento: Cypress.moment().add({ days: 1 }).format('DD/MM/YYYY'),
+                        data_transacao: Cypress.moment().format('DD/MM/YYYY'),
+                        descricao: "desc",
+                        envolvido: "inter",
+                        status: true,
+                        tipo: "REC",
+                        valor: "123",
+                    }
+                }).as('response')
+            })
+        cy.get('@response').its('status').should('be.equal', 201)
+        cy.get('@response').its('body.id').should('exist')
 
     })
-    it('Should get balance', () => {
+    it.skip('Should get balance', () => {
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+           // headers: { Authorization: `JWT ${token}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('534.00')
+        })
 
-    })
-    it('Should remove a transaction', () => {
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            //headers: { Authorization: `JWT ${token}` },
+            qs: { descricao: 'Movimentacao 1, calculo saldo' }
+        }).then(res => {
+            console.log(res.body[0])
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'PUT',
+               // headers: { Authorization: `JWT ${token}` },
+                body: {
+                    status: true,
+                    data_transacao: Cypress.moment(res.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: Cypress.moment(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+                    descricao: res.body[0].descricao,
+                    envolvido: res.body[0].envolvido,
+                    valor: res.body[0].valor,
+                    conta_id: res.body[0].conta_id
+                }
+            }).its('status').should('be.equal', 200)
+        })
 
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+           // headers: { Authorization: `JWT ${token}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
     })
+    
+    it.skip('Should remove a transaction', () => {
+        cy.request({
+            url: '/transacoes',
+            method: 'GET',
+            // headers: { Authorization: `JWT ${token}` },
+            qs: { descricao: 'Movimentacao para exclusao' }
+        }).then(res => {
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'DELETE',
+                // headers: { Authorization: `JWT ${token}` }
+            }).its('status').should('be.equal', 204)
+        })
+    })
+
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,37 +25,62 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import loc from '../support/locators'
 
-Cypress.Commands.add('login', (user, passwd)=>{
+Cypress.Commands.add('login', (user, passwd) => {
     cy.visit('https://barrigareact.wcaquino.me/')
-        cy.get(loc.LOGIN.USER).type('yuri@1')
-        cy.get(loc.LOGIN.PASSWORD).type('12345')
-        cy.get(loc.LOGIN.BTN_LOGIN).click()
-        cy.get(loc.MESSAGE).should('contain','Bem vindo')
+    cy.get(loc.LOGIN.USER).type('yuri@1')
+    cy.get(loc.LOGIN.PASSWORD).type('12345')
+    cy.get(loc.LOGIN.BTN_LOGIN).click()
+    cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
 })
-Cypress.Commands.add('resetApp', ()=>{
+Cypress.Commands.add('resetApp', () => {
     cy.get(loc.MENU.SETTINGS).click()
     cy.get(loc.MENU.RESET).click()
 })
 
-Cypress.Commands.add('getToken', (user,passwd)=>{
+Cypress.Commands.add('getToken', (user, passwd) => {
     cy.request({
         method: 'POST',
         url: '/signin',
-        body:{
+        body: {
             email: user,
             redirecionar: false,
             senha: passwd
         }
     }).its('body.token').should('not.be.empty')
-        .then(token => 
-            {return token})
+        .then(token => { return token })
 })
-Cypress.Commands.add('resetRest', ()=>{
-    cy.getToken('yuri@1','12345').then(token=>{
-    cy.request({
-        method: 'GET',
-        url: '/reset',
-        headers:{Authorization:`JWT ${token}`}
+Cypress.Commands.add('resetRest', () => {
+    cy.getToken('yuri@1', '12345').then(token => {
+        cy.request({
+            method: 'GET',
+            url: '/reset',
+            headers: { Authorization: `JWT ${token}` }
+        }).its('status').should('be.equal',200)
+    })
+})
+
+Cypress.Commands.add('getContaByName', name => {
+    cy.getToken('yuri@1', '12345').then(token => {
+        cy.request({
+            method: 'GET',
+            url: '/contas',
+            headers: { Authorization: `JWT ${token}` },
+            qs: {
+                nome: name
+            }
+        }).then(res => {
+            return res.body[0].id
         })
-    }).its('status').should('be.equal', 200)
+    })
+})
+Cypress.Commands.overwrite('request', (originalFn, ...options) => {
+    if (options.length === 1) {
+        if (Cypress.env('token')) {
+            options[0].headers = {
+                Authorization: `JWT ${Cypress.env('token')}`
+            }
+        }
+    }
+
+    return originalFn(...options)
 })
